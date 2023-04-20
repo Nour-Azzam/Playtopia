0a5b2e268b2c3192c09504b406c05e1f9e82e948
Nour Azzam <azzamnour2002@gmail.com>
2023-04-20 19:37:10 +0300

    create homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { GamesListComponent } from './games-list/games-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomepageComponent},
+  {path: 'games-list', component: GamesListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
